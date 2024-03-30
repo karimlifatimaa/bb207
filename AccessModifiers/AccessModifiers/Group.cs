4aa18a00d38f597be85ce8223ee9d122e556dbba
karimlifatimaa <bd7tzqw95@code.edu.az>
2024-03-31 01:00:23 +0400

    AccessModifiers

---
+++ AccessModifiers/AccessModifiers/Group.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class Group
+    {
+        public string No { get; set; }
+        public Student[] Students = new Student[] { };
+        private int StudentLimit;
+        public int _studentLimit
+        {
+            get
+            {
+                return _studentLimit;
+            }
+            set
+            {
+                if (value >= 0 && value <= 20)
+                {
+                    _studentLimit = value;
+                }
+                else
+                {
+                    Console.WriteLine("StudentLimit 0-dan kicik ve ya 20-den boyuk ola bilmez.");
+                }
+            }
+        }
+      
+        public Group(string no,int studentLimit)
+        {
+            
+            if(no.Length == 5 && char.IsUpper(no[0]) && char.IsUpper(no[1]) && char.IsDigit(no[2]) && char.IsDigit(no[3]) && char.IsDigit(no[4]))
+            {
+                No = no;
+
+            }
+            else
+            {
+                Console.WriteLine("No deyeri 2 boyuk herfle baslamalı ve sonrasında 3 reqem olmalıdır (AB204 kimi)");
+            }
+            if (studentLimit <= 20)
+            {
+                StudentLimit = studentLimit;
+            }
+            else
+            {
+                Console.WriteLine("20den boyukdur");
+
+            }
+
+
+        }
+        public void AddStudent(Student student)
+        {
+            if (Students.Length < StudentLimit)
+            {
+                Array.Resize(ref Students, Students.Length + 1);
+                Students[Students.Length - 1] = student;
+                Console.WriteLine("Student elave edildi ");
+            }
+            else
+            {
+                Console.WriteLine("Student elave etmek olmadi ");
+            }
+        }
+        public void ShowAllStudents()
+        {
+            for (int i = 0; i < Students.Length; i++)
+            {
+                Console.WriteLine($"Fullname is {Students[i].FullName} ");
+            }
+        }
+        public Student[] FilteredStudent(string fullName)
+        {
+            Student[] filteredStudent= new Student[] { };
+            for(int i = 0;i < Students.Length; i++)
+            {
+                if (Students[i].FullName == fullName)
+                {
+                    Array.Resize(ref filteredStudent, filteredStudent.Length + 1);
+                    filteredStudent[filteredStudent.Length - 1] = Students[i];
+                }
+                else
+                {
+                    Console.WriteLine("Tapilmadi");
+                }
+            }
+            return filteredStudent;
+        }
+    }
+}
---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "";
+           
+            string no;
+            int limit;
+            
+            do
+            {
+                Console.WriteLine("No daxil et 2 boyuk herfle baslamalı ve sonrasında 3 reqem olmalıdır (AB204 kimi)");
+                no = Console.ReadLine();
+
+            } while (!(no.Length == 5 && char.IsUpper(no[0]) && char.IsUpper(no[1]) && char.IsDigit(no[2]) && char.IsDigit(no[3]) && char.IsDigit(no[4])));
+            do
+            {
+                Console.WriteLine("Student sayini daxil et");
+
+            } while (!int.TryParse(Console.ReadLine(), out limit) || limit > 20 || limit < 0);
+            Console.WriteLine("Group yaradildi ");
+
+            Group group = new Group(no, limit);            
+            do
+            {
+                
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Menyu");
+                Console.WriteLine("1.Telebe elave et");
+                Console.WriteLine("2.Butun telebelere bax");
+                Console.WriteLine("3.Telebeler uzre axtaris et");
+                Console.WriteLine("0.Proqram bitti");
+                Console.WriteLine("Secimi et");
+                Console.WriteLine("-------------------------------");
+
+                answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "1":
+
+                        Console.WriteLine("FullNami daxil et :");                        
+                        string fullName = Console.ReadLine();
+
+                        Console.WriteLine("GroupNo daxil et : ");
+                        string groupNo = Console.ReadLine();
+
+                        string pointStr = "";
+                        double point;
+                        do
+                        {
+                            Console.WriteLine("AvgPoint daxil et : ");
+                            pointStr = Console.ReadLine();
+
+                        } while (!double.TryParse(pointStr, out point));
+                        Student student = new Student(fullName,groupNo,point);
+                        group.AddStudent(student);
+                        
+                        break;
+                    case "2":
+                        group.ShowAllStudents();
+                        break; 
+                    case "3":
+                        Console.WriteLine("axtardiginiz fullname i daxil et");
+                        string fullname= Console.ReadLine();
+                        Student[] filtered = group.FilteredStudent(fullname);
+                        foreach (var studet in filtered)
+                        {
+                            Console.WriteLine($"FulName: {studet.FullName}, Grup No: {studet.GroupNo}, AvgPoint: {studet.AvgPoint}");
+                        }
+                        break;
+                    case "0":
+                        Console.WriteLine("Proqram bitti");
+                        break;
+                    default: Console.WriteLine("Duzgun secim et");
+                        break ;
+                }
+
+            } while (answer!="0" );
+
+        }
+    }
+}
